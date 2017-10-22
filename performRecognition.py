fe2493bdfa02ce61cb43aa804ee1cabb65a5d4b5
davidchen <davidchen@gliacloud.com>
2017-10-22 18:11:36 +0800

    update

--- performRecognition.py
+++ performRecognition.py
@@ -7,6 +7,13 @@ from skimage.feature import hog
 import numpy as np
 import argparse as ap
 
+
+def show_image(im):
+    cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
+    cv2.imshow("Resulting Image with Rectangular ROIs", im)
+    cv2.waitKey()
+
+
 # Get the path of the training set
 parser = ap.ArgumentParser()
 parser.add_argument("-c", "--classiferPath", help="Path to Classifier File", required="True")
@@ -16,15 +23,24 @@ args = vars(parser.parse_args())
 # Load the classifier
 clf, pp = joblib.load(args["classiferPath"])
 
-# Read the input image 
+# Read the input image
 im = cv2.imread(args["image"])
-
-# Convert to grayscale and apply Gaussian filtering
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
+im_th = cv2.Canny(im_gray,100,200)
+# laplacian = cv2.Laplacian(im_gray, cv2.CV_64F)
+# show_image(laplacian)
+
+# Convert to grayscale and apply Gaussian filtering
+#im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+#im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
+
+# show_image(im_gray)
 # Threshold the image
-ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+# ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
+
+# show_image(im_th)
 
 # Find contours in the image
 _, ctrs, _ = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -35,8 +51,18 @@ rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 # For each rectangular region, calculate HOG features and predict
 # the digit using Linear SVM.
 for rect in rects:
+    if 15 < rect[2] < 50 and 25 < rect[3] < 50:
+        pass
+    else:
+        continue
+
+    if 440 < rect[0] < 620 and 125 < rect[1] < 165:
+        pass
+    else:
+        continue
+
     # Draw the rectangles
-    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
+    cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
     # Make the rectangular region around the digit
     leng = int(rect[3] * 1.6)
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
@@ -49,8 +75,10 @@ for rect in rects:
     roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
     roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
     nbr = clf.predict(roi_hog_fd)
+    # print nbr
     cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
 cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey()
+
